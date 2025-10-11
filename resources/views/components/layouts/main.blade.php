4271ca11d7b56824f7ca0ebe2e7d882da55e0298
Ali Vaghey <a.vaghey0@gmail.com>
2025-10-11 17:04:18 +0330

    sAt up some routes

--- resources/views/components/layouts/main.blade.php
+++ resources/views/components/layouts/main.blade.php
@@ -3,18 +3,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Personal Accounting</title>
-</head>
-<body class="relative">
 
-    <img class="absolute -z-50 w-screen h-screen" src="{{ asset('background.jpg') }}" alt="background">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="relative m-0 p-0">
+
+    <img class="absolute -z-50 !w-screen !h-screen" src="{{ asset('background.jpg') }}" alt="background">
 
     <div>
         <main>
             {{ $slot }}
         </main>
     </div>
-    <nav>
+    <nav class="absolute left-0 bottom-0 w-screen h-20 backdrop-blur bg-white/5 z-50">
+        <div class="flex justify-around">
 
+        </div>
     </nav>
 </body>
 </html>
