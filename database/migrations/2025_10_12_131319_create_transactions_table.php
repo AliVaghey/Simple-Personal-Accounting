7857213650aad1523934e798fc0e072c5dc8ae25
Ali Vaghey <a.vaghey0@gmail.com>
2025-10-12 16:55:54 +0330

    set up the transaction model

--- database/migrations/2025_10_12_131319_create_transactions_table.php
+++ database/migrations/2025_10_12_131319_create_transactions_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->boolean('is_expense');
+            $table->decimal('amount', 10, 0);
+            $table->string('description')->nullable();
             $table->timestamps();
         });
     }
