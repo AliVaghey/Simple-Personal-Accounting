91770b0fc63fe75be278304af9b5c5fe75f9c392
Ali Vaghey <a.vaghey0@gmail.com>
2025-10-14 10:39:10 +0330

    improved seeders

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
@@ -58,5 +59,20 @@ class User extends Authenticatable
             ->take(2)
             ->map(fn ($word) => Str::substr($word, 0, 1))
             ->implode('');
+    }
+
+    public function tags(): HasMany
+    {
+        return $this->hasMany(Tag::class);
+    }
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    public function transTags(): HasMany
+    {
+        return $this->hasMany(TransTag::class);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Tag;
 use App\Models\Transaction;
+use App\Models\TransTag;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -24,7 +25,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@test.com'
         ]);
 
-        Tag::factory()
+        $tags = Tag::factory()
             ->count(20)
             ->create([
                 'user_id' => $user->id,
@@ -39,5 +40,13 @@ class DatabaseSeeder extends Seeder
         $this->call([
             TransTagSeeder::class
         ]);
+
+        $total_transactions = $user->transactions()->count();
+        foreach ($tags as $tag) {
+            $total_transTags = TransTag::where('tag_id', $tag->id)->where('user_id', $user->id)->count();
+            $tag->usage_percentage = intval($total_transTags / $total_transactions  * 100);
+            $tag->transactions_count = $total_transTags;
+            $tag->save();
+        }
     }
 }
