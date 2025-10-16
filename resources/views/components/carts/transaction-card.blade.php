99980ac06d495d0f7e549eeba48e2896c4366d63
Ali Vaghey <a.vaghey0@gmail.com>
2025-10-16 12:12:20 +0330

    added the transaction's date

--- resources/views/components/carts/transaction-card.blade.php
+++ resources/views/components/carts/transaction-card.blade.php
@@ -1,6 +1,7 @@
 <div {{ $attributes }} class="border  rounded-xl px-4 py-2 {{ $trans->is_expense ? 'bg-green-500/10 border-green-500' : 'bg-red-500/10 border-red-500'  }}">
-    <div class=" {{ $trans->is_expense ? 'text-green-500' : 'text-red-500'  }}">
-        {{ number_format($trans->amount) }}
+    <div class="w-full flex justify-between {{ $trans->is_expense ? 'text-green-500' : 'text-red-500'  }}">
+        <div>{{ number_format($trans->amount) }}</div>
+        <div>{{ shamsi($trans->created_at) }}</div>
     </div>
     <div class="py-2 {{ $trans->is_expense ? 'text-green-500' : 'text-red-500'  }}">
         {{ $trans->description }}
@@ -8,10 +9,10 @@
     <div class="flex flex-wrap justify-around gap-2 items-center  px-3 ">
         @foreach($trans->transTags as $transTags)
         @php($tag = $transTags->tag)
-            <div 
+            <div
                 class=" px-3 py-1 rounded-xl border border-white/20 shadow-sm"
                 style="background-color: {{ $tag->color }}20; border-color: {{ $tag->color }};">
-                <div 
+                <div
                     class="font-semibold text-sm"
                     style="color: {{ $tag->color }};">
                         {{ $tag->name }}
