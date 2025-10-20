12796418ba10848645e32a42b698968c77ec2d58
Ali Vaghey <a.vaghey0@gmail.com>
2025-10-20 17:12:45 +0330

    kansdfkn;a

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,19 +1,20 @@
 <x-layouts.main>
 
     <div class="py-9 flex flex-col items-center justify-center gap-4">
-        <x-carts.dashboard-cart :title="'روز'"
+        <x-carts.dashboard-cart :title="'امروز'"
                                 :income="$currentDayStats['incomes']"
                                 :expense="$currentDayStats['expenses']"
                                 :percentage="json_encode($currentDayStats['percentage'])"
                                 :tags="$currentDayStats['tags']"/>
 
-        <x-carts.dashboard-cart :title="'ماه'"
+        <x-carts.dashboard-cart :title="'این ماه'"
                                 :income="$currentMonthStats['incomes']"
                                 :expense="$currentMonthStats['expenses']"
                                 :percentage="json_encode($currentMonthStats['percentage'])"
                                 :tags="$currentMonthStats['tags']"
         />
 
+        <div class="text-white text-2xl mt-5">آخرین تراکنش ها</div>
         <livewire:transaction-list />
 
     </div>
